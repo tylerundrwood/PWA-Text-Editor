8e7f603ac39a56669d066c7f929213335b687f95
Tyler Underwood <twood9600@yahoo.com>
2023-08-27 12:02:37 -0400

    created js database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,41 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+  export const putDb = async (id, content) => {
+    // create connection to database
+    const jateDB = await openDB("jate", 1);
+  
+    // create new transaction and the privileges we want
+    const transact = jateDB.transaction("jate", "readwrite");
+  
+    // open up object store we want
+    const store = transact.objectStore("jate");
+  
+    // add() to add data to db
+    const request = store.put({id: id, content: content} );
+  
+    // confirm the request worked
+    const result = await request;
+    return result;
+  };
+  
+  // TODO: Add logic for a method that gets all the content from the database
+  export const getDb = async () => {
+    // create connection to database
+    const jateDB = await openDB("jate", 1);
+  
+    // create new transaction and the privileges we want
+    const transact = jateDB.transaction("jate", "readonly");
+  
+    // open up object store we want
+    const store = transact.objectStore("jate");
+  
+    // getAll() built in method to get all data in db
+    const request = store.getAll(1);
+  
+    // confirm the request worked
+    const result = await request;
+    return result?.value;
+  };
 
 initdb();
